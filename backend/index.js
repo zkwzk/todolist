86b1587a9a5da127c296ccd91ceeb45b82891048
Zhengkun Wang <zkwang@thoughtworks.com>
2022-09-11 23:53:18 +0800

    [ZK] added cors config for frontend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import {create, done, getAll} from './apis/items.js'
+import cors from "cors";
 
 const app = express();
 const port = 3001;
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 app.get('/', (req, res) => {
     res.send(getAll())
